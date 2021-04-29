c4d8e93c9460b4cf1a2164ff28ef62b41120d76a
Sơn Nguyễn <n.n.son.it.cv@gmail.com>
2021-04-29 10:43:38 +0700

    Ecommerce

---
+++ asset/js/index.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+    $('.slide-header').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<button class="btn-slide btn-slide-prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/></svg></button>','<button class="btn-slide btn-slide-next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/></svg></button>'],
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        },
+
+        autoplay:true,
+        autoplayTimeout:10000,
+        autoplayHoverPause:true
+    });
+
+    
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[1000])
+    });
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    });
+
+    var owlSlideLogo = $('.slide-logo')
+    owlSlideLogo.owlCarousel({
+        loop:true,
+        nav:true,
+        margin:0,
+        navText:['<button class="btn-slide btn-slide-logo-prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/></svg></button>','<button class="btn-slide btn-slide-logo-next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/></svg></button>'],
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            800:{
+                items:4
+            },
+            1000:{
+                items:5
+            }
+            ,
+            1200:{
+                items:6
+            }
+        },
+    });
+
+    var owlSlideProduct = $('.slide-product')
+    owlSlideProduct.owlCarousel({
+        loop:true,
+        nav:true,
+        margin:0,
+        navText:['<button class="btn-slide-product-prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/></svg></button>','<button class="btn-slide-product-next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/></svg></button>'],
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            800:{
+                items:3
+            },
+            1200:{
+                items:5
+            }
+        },
+    });
+});
